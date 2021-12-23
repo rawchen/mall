edaaa9d53a23f568deabdae4c07753ee5362d19c
RawChen <2221999792@qq.com>
2021-12-23 23:59:36 +0800

    【新增】网关配置

--- mall-gateway/src/main/java/com/rawchen/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/rawchen/gateway/MallGatewayApplication.java
@@ -2,7 +2,9 @@ package com.rawchen.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallGatewayApplication {
 
