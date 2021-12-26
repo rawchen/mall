68f26ba2a58463af5681bd993df697bf8f510e0c
RawChen <2221999792@qq.com>
2021-12-26 23:21:14 +0800

    【新增】Feign测试

--- mall-member/src/main/java/com/rawchen/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/rawchen/mall/member/MallMemberApplication.java
@@ -3,7 +3,9 @@ package com.rawchen.mall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.rawchen.mall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MallMemberApplication {
