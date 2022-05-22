5fec89c0bba1700884fbbcb16f17aaa45f7ab6f2
RawChen <2221999792@qq.com>
2022-05-23 00:28:01 +0800

    【新增】网关服务

---
+++ mall-gateway/src/main/java/com/rawchen/gateway/config/SentinelGateWayConfig.java
@@ -0,0 +1,30 @@
+package com.rawchen.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.fastjson.JSON;
+import com.rawchen.common.exception.BizCodeEnum;
+import com.rawchen.common.utils.R;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * <p>Title: SentinelGateWayConfig</p>
+ * Description：
+ * date：2022/2/10 17:57
+ */
+@Configuration
+public class SentinelGateWayConfig {
+
+
+
+	public SentinelGateWayConfig(){
+		GatewayCallbackManager.setBlockHandler((exchange, t) ->{
+			// 网关限流了请求 就会回调这个方法
+			R error = R.error(BizCodeEnum.TO_MANY_REQUEST.getCode(), BizCodeEnum.TO_MANY_REQUEST.getMsg());
+			String errJson = JSON.toJSONString(error);
+			Mono<ServerResponse> body = ServerResponse.ok().body(Mono.just(errJson), String.class);
+			return body;
+		});
+	}
+}
---
+++ mall-gateway/src/test/java/com/rawchen/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawchen.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
