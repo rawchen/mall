d54fe2adc94386c3e77efaab846205ce7b37894f
RawChen <2221999792@qq.com>
2022-05-22 22:38:44 +0800

    【新增】人人开源快速搭建框架

---
+++ renren-fast/src/main/java/io/renren/config/MallCorsConfiguration.java
@@ -0,0 +1,31 @@
+package io.renren.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * 配置跨域
+ *
+ * @author RawChen
+ * @date 2021-12-30 11:12
+ */
+@Configuration
+public class MallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
